a5e5e3fc14fb026e41e97438da54251259f2fb92
Sheracore <m.ghaffari662@gmail.com>
2023-08-21 12:39:24 +0330

    feat: add mssql to project

---
+++ payvast_interview/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for payvast_interview project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'payvast_interview.settings')
+
+application = get_asgi_application()
